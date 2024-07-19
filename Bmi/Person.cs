b8900280342454655ba614f573c65bf5c58a3ebc
adi4032 <110769508+adi4032@users.noreply.github.com>
2024-07-19 23:12:28 +0530

    Add files via upload

---
+++ Bmi/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Bmi
+{
+    internal class Person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public double height { get; set; }
+        public int weight { get; set; }
+
+        public Person(int id, string name, int age)
+        : this(id, name, age, 5, 150)
+        {
+        }
+        public Person(int id, string name, int age, double height, int weight)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public double CalculateBmi()
+        {
+            double heightInMeters = height * 0.3048;
+            double bmi = weight / (heightInMeters * heightInMeters);
+            return bmi;
+        }
+
+        public string PrintDetails()
+        {
+            return $"ID: {id}, Name: {name}, Age: {age}, Height: {height}ft, Weight: {weight}kg";
+        }
+
+        public string GetBodyType()
+        {
+            double bmi = CalculateBmi();
+
+            if (bmi < 18.5)
+            {
+                return "Underweight";
+            }
+            else if (bmi >= 18.5 && bmi < 25)
+            {
+                return "Normal";
+            }
+            else
+            {
+                return "Overweight";
+            }
+
+        }
+
+    }
+}
---
+++ Bmi/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Person[] persons = new Person[4]
+                {
+                new Person(101, "Ram", 20, 5.5, 50),  
+                new Person(102, "Aditya", 30, 6.0, 60),
+                new Person(103, "Sham", 40, 5.0, 70),
+                new Person(104, "Sachin", 50, 6.0, 80)
+                };
+
+           
+                foreach (var person in persons)
+                {
+                    PrintDetails(person);
+                }
+
+                Person highestBmiPerson = GetPersonWithHighestBmi(persons);
+                Console.WriteLine($"Person with highest BMI: {highestBmiPerson.name}");
+            }
+
+        }
+        static void PrintDetails(Person person)
+            {
+                Console.WriteLine(person.PrintDetails());
+                Console.WriteLine($"BMI is {person.CalculateBmi():F2}"); 
+                Console.WriteLine($"Body Type: {person.GetBodyType()}");
+                Console.WriteLine();
+            }
+
+            static Person GetPersonWithHighestBmi(Person[] persons)
+            {
+                Person highestBmiPerson = persons[0];
+
+                foreach (Person p in persons)
+                {
+                    if (p.CalculateBmi() > highestBmiPerson.CalculateBmi())
+                    {
+                        highestBmiPerson = p;
+                    }
+                }
+
+                return highestBmiPerson;
+            }
+        }
+    }
