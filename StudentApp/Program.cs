1ab943b92cb98d4f060e99c1131aa515bc084651
adi4032 <110769508+adi4032@users.noreply.github.com>
2024-07-20 23:28:10 +0530

    Add files via upload

---
+++ StudentApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(101, "aditya", 8.5f);
+
+            student.DisplayInfo();
+            
+            double percentage = student.GetPercentage();
+            Console.WriteLine($"Percentage: {percentage}%");
+            student.SetStudentCgpa(11.0f);
+            student.SetStudentRollNumber(-1);
+            student.SetStudentName("111");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentApp/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StudentApp
+{
+    internal class Student
+    {
+        private int RollNo;
+        private string Name;
+        private float Cgpa;
+        
+        public Student(int RollNo, string Name, float Cgpa)
+        {
+            this.RollNo = RollNo;
+            this.Name = Name;
+            this.Cgpa = Cgpa;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Roll No: {RollNo}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"CGPA: {Cgpa}");
+        }
+        public void SetStudentCgpa(float Cgpa)
+        {
+            if (1 <= Cgpa && Cgpa <= 10)
+            {
+                this.Cgpa = Cgpa;
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Cgpa");
+            }
+        }
+        public void SetStudentRollNumber(int RollNo)
+        {
+            if (RollNo > 0)
+            {
+                this.RollNo = RollNo;
+            }
+            else
+            {
+                Console.WriteLine("Roll Number can't be Negative");
+            }
+        }
+
+        public void SetStudentName(string Name)
+        {
+           Name = (Name.Length < 5) ? "" : Name;
+        }
+        public double GetPercentage()
+        {
+            return Cgpa * 10; // Assuming CGPA is out of 10
+        }
+
+    }
+}
