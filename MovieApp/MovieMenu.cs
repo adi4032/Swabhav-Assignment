76af28f6b8143c72e17dd8c8a71c4694a09bc72f
adi4032 <110769508+adi4032@users.noreply.github.com>
2024-07-30 22:03:54 +0530

    Add files via upload

---
+++ MovieApp/MovieMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp
+{
+    public class MovieMenu
+    {
+        MovieMenu menu = new MovieMenu();
+        public static void StartMenu()
+        {
+            bool continueProgram = true;
+
+            while (continueProgram)
+            {
+                Console.Clear();
+                Console.WriteLine("Movie Management Menu:");
+                Console.WriteLine("1. Add Movie");
+                Console.WriteLine("2. Delete Movie");
+                Console.WriteLine("3. Display Movies");
+                Console.WriteLine("4. Exit");
+
+                string choice = Console.ReadLine();
+                MovieManager movieManager=new MovieManager();
+                switch (choice)
+                {
+                    case "1":
+                        MovieManager.AddMovie();
+                        break;
+                    case "2":
+                        MovieManager.DeleteMovie();
+                        break;
+                    case "3":
+                        MovieManager.DisplayMovies();
+                        break;
+                    case "4":
+                        continueProgram = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+
+                if (continueProgram)
+                {
+                    Console.Write("Do you want to continue? (y/n): ");
+                    string continueChoice = Console.ReadLine();
+                    continueProgram = continueChoice.Equals("y", StringComparison.OrdinalIgnoreCase);
+                }
+            }
+        }
+    }
+}
+
+
---
+++ MovieApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using MovieApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ArrayList movies = new ArrayList();
+        //movies.Add(new MovieMenu("The Shawshank Redemption", "Drama", 1994));
+        //movies.Add(new MovieMenu("Maharaja", "Action", 2024));
+        MovieMenu.StartMenu();
+    }
+}
